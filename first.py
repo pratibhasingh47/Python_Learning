42a9e15c96aec4a4f44d377f3a45fc8ddcc4cf3d
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-17 23:38:47 +0530

    variables

--- first.py
+++ first.py
@@ -1,2 +1,32 @@
-print("Hello World")
+print("Hello World" , "Hello Pratibha")
 print("Hello Pratibha")
+print(23)
+print(23.5)
+print(10+20)
+print(10-20)
+
+name = "Pratibha"
+print(name)
+
+age = 20
+print(age)
+
+price = 25.99
+print(price)
+
+# age = 30
+# print(age)
+print("My name is", name, "and my age is", age)
+print(type(name))
+print(type(age))
+print(type(price))
+
+# a = None
+# print(a)
+
+a = 100
+b = 200
+sum = a + b
+diff = a - b
+print(sum)
+print(diff) 

f602a668da048b6a169b2ae8636f750962d72664
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-21 14:34:10 +0530

    Add sorting and palindrome check to lists.py, and tuple operations to tuple.py

--- lists.py
+++ lists.py
@@ -41,4 +41,29 @@ list1.sort(reverse=True)
 print(list1)
 # print(list1.sort()) #None
 
+fruits = ["apple", "banana", "cherry" , "orange", "kiwi", "mango"]
 
+fruits.sort()
+print(fruits)
+
+movies = []
+mov1 = input("Enter 1st movie: ")
+mov2 = input("Enter 2nd movie: ")
+mov3 = input("Enter 3rd movie: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
+
+list1 = [1,2,3,2,1]
+list2 = [1,2,3]
+list3 = ['m', 'o', 'm']
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if list1 == copy_list1:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
---
+++ tuple.py
@@ -0,0 +1,26 @@
+tup = (87, 75, 92, 100, 99)
+print(tup)
+print(type(tup))
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+print(tup[4])
+print(len(tup))
+print(tup[1:3])
+print(tup[1:])
+print(tup[:3])
+
+print(tup.count(87))
+print(tup.index(87))
+# tup[0] = 90 #TypeError: 'tuple' object does not support item assignment
+t = (1,)
+print(t)
+print(type(t))
+
+tup1 = ("Hello")
+print(tup1)
+print(type(tup1))
+
+
+

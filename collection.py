fc03671b4820f6843d764e8d4585f8e0106ab458
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-21 15:04:53 +0530

    Add dictionary operations to collection.py

---
+++ collection.py
@@ -0,0 +1,18 @@
+collection = {1,2,2,"hello", "world", "hello", 1}
+print(collection)
+print(type(collection))
+print(len(collection))
+collection.add(3)
+print(collection)
+collection.remove(2)
+print(collection)
+collection.discard(2)
+print(collection)
+collection.clear()
+print(collection)
+
+col1 = {}
+print(type(col1))
+
+col2 = set()
+print(type(col2))

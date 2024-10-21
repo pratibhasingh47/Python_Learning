740d6c56b7247523185b07716ae3055c3fbe1ba1
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-21 14:04:15 +0530

    Add lists and print statements to lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+marks = [90, 80, 70, 60]
+grades = ["A", "B", "C", "D"]
+print(marks)
+print(grades)
+print(type(marks))
+print(type(grades))
+print(marks[0])
+print(grades[0])
+print(len(marks))
+print(len(grades))
+
+students = ["John", "Doe", 25, 90.5]
+print(students)
+print(students[0])
+print(students[1])
+print(students[2])
+print(students[3])

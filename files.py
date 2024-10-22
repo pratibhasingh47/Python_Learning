ed7adb4e463206a59bfd0d487e2d870223bf6c27
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-22 20:05:29 +0530

    Add file operations to files.py

--- files.py
+++ files.py
@@ -25,11 +25,35 @@ os.remove("sample.txt")
 
 # with open("practice.txt", "w") as f:
 #     f.write("This is practice writing JAVA")
-with open("practice.txt", "w") as f:
+# with open("practice.txt", "w") as f:
+#     data = f.read()
+
+# new_data = data.replace("JAVA", "Python")
+# print(new_data)
+
+# with open("practice.txt", "w") as f:
+#     f.write(new_data)
+
+
+count = 0   
+with open("practice.txt", "r") as f:
     data = f.read()
+    print(data)
 
-new_data = data.replace("JAVA", "Python")
-print(new_data)
+    """
+    num = ""
+    for i in range(len(data)):
+        if(data[i] ==","):
+            print(num)
+            num = ""
+        else:
+            num += data[i]
+    """
 
-with open("practice.txt", "w") as f:
-    f.write(new_data)
+    nums = data.split(",")
+    print(nums)
+    for val in nums:
+        if(int(val) % 2 == 0):
+            count += 1
+
+print(count)
